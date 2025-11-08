f54e011a4472b8dd33a1c20fc55f804302059b82
a2yomi <a2yomidiscord@gmail.com>
2025-11-08 21:22:17 +0300

    fix: app config

--- app.config.ts
+++ app.config.ts
@@ -6,12 +6,12 @@ export type AppConfig = {
 };
 
 const DevConfig: AppConfig = {
-  chat_id: -1002975807882,
+  chat_id: 7744053901,
   allowedChannelId: "1408424880608907364",
 };
 
 const ProdConfig: AppConfig = {
-  chat_id: 7744053901,
+  chat_id: -1002975807882,
   allowedChannelId: "1420841102570688625",
 };
 
