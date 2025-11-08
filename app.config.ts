d7e162c2ff919dc132939c46f969f78284954a05
a2yomi <a2yomidiscord@gmail.com>
2025-11-08 16:58:20 +0300

    chore: update config

--- app.config.ts
+++ app.config.ts
@@ -1,3 +1,19 @@
-export const AppConfig = {
-  chat_id: 7744053901,
+import { Env } from "./shared/config";
+
+export type AppConfig = {
+  chat_id: number;
+  allowedChannelId: string;
 };
+
+const DevConfig: AppConfig = {
+  chat_id: -1002975807882,
+  allowedChannelId: "1408424880608907364",
+};
+
+const ProdConfig: AppConfig = {
+  chat_id: 7744053901,
+  allowedChannelId: "1420841102570688625",
+};
+
+export const AppConfig: AppConfig =
+  Env.AppEnv === "dev" ? DevConfig : ProdConfig;
